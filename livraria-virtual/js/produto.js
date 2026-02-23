ee44ae1b177f6fa9953cb312477cb0c2304a1b0a
Cecília Faria <cecilia.sfaria12@gmail.com>
2026-02-23 10:48:48 -0300

    Add files via upload

---
+++ livraria-virtual/js/produto.js
@@ -0,0 +1,15 @@
+// ===========================
+// Página de Produto
+// ===========================
+
+function changeImage(src) {
+    document.getElementById('product-img').src = src;
+}
+
+function addToCart() {
+    alert("Produto adicionado ao carrinho!");
+}
+
+function buyNow() {
+    alert("Redirecionando para o pagamento...");
+}
